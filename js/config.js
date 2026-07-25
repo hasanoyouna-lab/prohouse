25424f07bb69fc8fb62b7be4fa3992502f994d1d
Pro House Dev <hasanoyouna@gmail.com>
2026-07-25 22:31:18 +0300

    Wire live Apps Script /exec URL into config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // إعدادات الاتصال بالباك اند. الصق رابط /exec من Apps Script هون بعد النشر (راجع apps-script/SETUP.md).
-const API_URL = "";
+const API_URL = "https://script.google.com/macros/s/AKfycbykhtn0VUleuPkNYAKutt6AFrpl-atN5dmruiRGTSkK8ejYZxzbsZ71AZyDQD_LMbe_/exec";
 
 const APP_VERSION = "2.0.0";
